a1199650dcda23f0765670532716bd714699c13f
Langster2323 <langahkeem@gmail.com>
2019-11-14 11:04:13 -0600

    Created list and profile component

---
+++ src/components/Profile.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { AuthConsumer } from '../authContext';
+
+const Profile = () => {
+  return (
+    <AuthConsumer>
+      {({ user }) => {
+        <div>
+          <h2>User Profile</h2>
+          <ul>
+            <li>ID: {user.id}</li>
+            <li>Email: {user.email}</li>
+            <li>Role: {user.role}</li>
+          </ul>
+        </div>
+      }}
+    </AuthConsumer>
+  );
+}
+
+export default Profile;
