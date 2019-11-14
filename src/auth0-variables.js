0e68e866081362b002b4ca69bb43c3a9f93d761f
Langster2323 <langahkeem@gmail.com>
2019-11-14 07:41:50 -0600

    Configuring library

---
+++ src/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: "blog-samples.auth0.com",
+  roleUrl: "https://rbac-app/role",
+  clientId: "OnfZ50CbGRPa0hS6OXlHFrPEGUjA3O5u",
+  callbackUrl: "http://localhost:3000/callback"
+};
